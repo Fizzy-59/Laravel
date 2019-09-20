517e4090e8590f4c09ac85d0ddf707ed3c8d3cf6
Fizzy-59 <rom.dev@live.fr>
2019-09-20 17:00:53 +0200

    mise en place de db

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,18 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/person', function()
+{
+    $person = 
+    [
+        'firstname' => 'Romain',
+        'last_name' => 'Devillez',
+    ];
+
+    return $person;
 });
+
+// Nous n'utiliserons pas cette route pour le tuto
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
